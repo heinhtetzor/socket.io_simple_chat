6e85ddc7bd1dedf6349d381b9d80352b3850a5d2
heinhtetzor <hhz18@icloud.com>
2020-02-27 15:52:48 +0800

    after all

--- index.js
+++ index.js
@@ -7,22 +7,16 @@ app.use('/chat', (req, res) => {
 })
 app.use(express.static('public'));
 
-const server = app.listen(process.env.PORT || 3000, () => console.log("connected"));
+const server = app.listen(3000, '0.0.0.0', () => console.log("connected"));
 
 //socket.io setup
 const io = socket(server);
-
-io.configure(function () { 
-    io.set("transports", a["xhr-polling"]); 
-    io.set("polling duration", 10); 
-  });
-  
 io.on('connection', function(socket){
     console.log("Connection has been made."+socket.id);
     socket.on('chat', function(data){
         io.sockets.emit('chat', data);
     })
     socket.on('typing', function(data){
-        socket.broadcast.emit('typing', data);
+        socket.broadcast.emit('typing', data)
     })
 })
